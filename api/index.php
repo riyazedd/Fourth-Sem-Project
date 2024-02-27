92ce8467c139a3372298cfd564e6fcdc50b2f7d3
riyazedd <riyaz.shrestha00@gmail.com>
2024-02-27 09:18:23 +0545

    Update

--- api/index.php
+++ api/index.php
@@ -24,4 +24,29 @@ if($server=="GET"){
     }else{
         echo "Error adding data";
     }
+}else if($server=="DELETE"){
+    $myEntireBody= file_get_contents('php://input');
+    $myBody= json_decode($myEntireBody);
+    $id=$myBody->id;
+    $sql="DELETE FROM students WHERE id=$id";
+    if(mysqli_query($conn,$sql)){
+        echo "User deleted succesfully";
+    }else{
+        echo "Error: data not deleted";
+    }
+}else if($server=="PUT"){
+    $myEntireBody=file_get_contents('php://input');
+    $myBody= json_decode($myEntireBody);
+    $id=$myBody->id;
+    $name=$myBody->name;
+    $address=$myBody->address;
+    $sql="UPDATE students SET name='$name', address='$address' WHERE id=$id";
+    if(mysqli_query($conn,$sql)){
+        echo "Updated Succesfully";
+    }else{
+        echo "Error: Data not Updated";
+    }
+    
+}else{
+    echo "Invalid request";
 }
